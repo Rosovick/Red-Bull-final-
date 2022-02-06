ebf459ae5356272573b3e954ef48833b375e4897
Sofia Rojas <rosovick1@gmail.com>
2022-02-05 21:18:40 -0300

    ajax

--- js/main.js
+++ js/main.js
@@ -38,3 +38,18 @@ for(i=0;i<linkCollapse.length;i++){
 }
 
 
+console.log("activo");
+document.querySelector('#botonAjax').addEventListener('click', datos)
+
+function datos(){
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.open('GET', 'archivo.txt', true);
+  xhttp.send();
+  xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+      // document.getElementById("demo").innerHTML = this.responseText;
+      document.querySelector('#respuesta').innerHTML = this.responseText;
+     }
+  }
+}
